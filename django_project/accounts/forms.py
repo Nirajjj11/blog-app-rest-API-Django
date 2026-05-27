c063474a54557d17c3d446fda05f4fbe752692ca
Your Name <nirajkumar09160@gmail.com>
2026-05-28 01:59:27 +0530

    blogs done

--- django_project/accounts/forms.py
+++ django_project/accounts/forms.py
@@ -3,11 +3,11 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
-      class Meta(UserCreationForm):
+      class Meta(UserCreationForm.Meta):
             model = CustomUser
-            fields = UserCreationForm.Meta.fields + ('name')
+            fields = UserCreationForm.Meta.fields + ('name',)
       
 class CustomUserChangeForm(UserChangeForm):
-      class Meta:
+      class Meta(UserChangeForm.Meta):
             model = CustomUser
             fields = UserChangeForm.Meta.fields
--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     
     # for custom app    
     "accounts.apps.AccountsConfig",
+    "posts",
 ]
 
 MIDDLEWARE = [
