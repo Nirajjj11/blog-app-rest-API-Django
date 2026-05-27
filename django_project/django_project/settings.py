b420f4a14e110f1473047fa3d4659a380829f095
Your Name <nirajkumar09160@gmail.com>
2026-05-28 00:57:10 +0530

    added the cstom user

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # for custom app    
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
