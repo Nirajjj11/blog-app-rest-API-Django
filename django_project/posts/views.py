754729986326520f18eb9cc63cd846eb4297a53f
Your Name <nirajkumar09160@gmail.com>
2026-05-28 23:17:53 +0530

    Custom settings for the authinticated posts and user have permission to update, see, or delete as the user type

--- django_project/posts/views.py
+++ django_project/posts/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Post
 from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly 
 
 class PostList(generics.ListCreateAPIView):
+      pagination_class = (IsAuthorOrReadOnly,)
       queryset = Post.objects.all()
       serializer_class = PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+      permission_classes = (IsAuthorOrReadOnly,)
       queryset = Post.objects.all()
       serializer_class = PostSerializer
